bc844309b985e893b9b6bf8f3018705bb64d24ed
shabyWoks <shaby1@outlook.com>
2018-02-22 01:21:04 +0530

    DF- 03 can be released

--- data-form.module.ts
+++ data-form.module.ts
@@ -13,6 +13,12 @@ import { TextAreaComponent } from './text-area/text-area.component';
 import { TextCheckService } from './service/text-check.service';
 import { TextRadioComponent } from './text-radio/text-radio.component';
 import { StatusComponent } from './status/status.component';
+import { DataFormService } from './service/data-form.service';
+import { Options } from './models/options';
+import { FormInputDataArgs } from './models/form-inputdata-arg';
+import { FormReturnDataArgs } from './models/form-return-data';
+import { InputPropArgs } from './models/input-prop';
+
 
 @NgModule({
   imports: [
@@ -33,10 +39,15 @@ import { StatusComponent } from './status/status.component';
   providers: [
     TextInputService,
     FileUploadService,
-    TextCheckService
+    TextCheckService,
+    DataFormService
   ],
   exports: [
-    DataFormComponent
+    DataFormComponent,
+    Options,
+    FormInputDataArgs,
+    FormReturnDataArgs,
+    InputPropArgs
   ]
 })
 export class DataFormModule { }
